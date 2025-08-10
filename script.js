5b048bb3107b25f183c2bc9d9850f3de884559d0
dhavi <222001371+dhav1i@users.noreply.github.com>
2025-08-10 15:37:12 +0300

    Add classic, modern and erase buttons

--- script.js
+++ script.js
@@ -1,12 +1,51 @@
-const containerElement = document.querySelector('div')
+const containerElement = document.querySelector('.container');
+const slider = document.querySelector('.grids-slider');
+const rangeValue = document.querySelector('.range-value');
+const classicBtn = document.querySelector('.classic');
+const modernBtn = document.querySelector('.modern');
+const eraseBtn = document.querySelector('.erase');
+const squares = document.querySelectorAll('.square');
 
-for(let i = 0; i < 256; i++) {
- const square = document.createElement('div')
-  square.classList.add('square')
-  containerElement.appendChild(square)
-  let opacity = 0.1
-  square.addEventListener('mouseenter', () => {
-    opacity += 0.1
-   square.setAttribute('style', `background-color: rgba(0, 0, 0, ${opacity})`)
-  })
+
+rangeValue.textContent = slider.value;
+
+slider.oninput = function() {
+    rangeValue.textContent = this.value;
+};
+
+let currentMode = "classic";
+
+function createGrid(num) {
+    let squareDimension = 480 / num;
+    
+    for(let i = 0; i < num * num; i++) {
+        const square = document.createElement('div')
+        square.classList.add('square');
+        containerElement.appendChild(square);
+        let opacity = 0;
+        
+        square.addEventListener('mouseenter', () => {
+            if(currentMode === "classic") {
+                opacity += 0.1
+                square.setAttribute('style', `background-color: rgba(0, 0, 0, ${opacity}); height: ${squareDimension}px; width: ${squareDimension}px`)
+            } else if(currentMode === "modern") {
+                const randomR = Math.floor(Math.random() * 255);
+                const randomG = Math.floor(Math.random() * 255);
+                const randomB = Math.floor(Math.random() * 255);
+                square.setAttribute('style', `background-color: rgb(${randomR},${randomG},${randomB}); height: ${squareDimension}px; width: ${squareDimension}px`)
+            }
+        });
+        eraseBtn.addEventListener('click', () => {
+            opacity = 0;
+            square.style.backgroundColor = "white";
+        });
+        square.style.height = `${squareDimension}px`;
+        square.style.width = `${squareDimension}px`;
+    }
 }
+
+modernBtn.addEventListener('click', () => {currentMode = "modern"});
+classicBtn.addEventListener('click', () => {currentMode = "classic"});
+
+window.onload = createGrid(16);
+
